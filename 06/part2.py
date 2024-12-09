586f41c83dc625aa66c935365362941c639ebfa1
Nick Dawson <37987430+NicholasDawson@users.noreply.github.com>
2024-12-09 23:42:05 +0000

    day 07 in progress

--- 06/part2.py
+++ 06/part2.py
@@ -3,55 +3,70 @@ from pprint import pprint
 from time import sleep
 from typing import List
 
-puzzle_input = open('./06/example', encoding='utf-8').read().splitlines()
+puzzle_input = open('./06/input', encoding='utf-8').read().splitlines()
 
 # get locations of everything
 x_max = len(puzzle_input) - 1
 y_max = len(puzzle_input[0]) - 1
-obstacles = []
-guard = None
+original_obstacles = []
+start = None
 
 for x in range(len(puzzle_input)):
     for y in range(len(puzzle_input[0])):
         match puzzle_input[x][y]:
             case '#':
-                obstacles.append((x, y))
+                original_obstacles.append((x, y))
             case '^':
-                guard = (x, y)
+                start = (x, y)
 
-# now move around the guard -- "play the game"
-positions = set()
-new_obstacles = set()
-guard_moving = 90
-#print(guard)
-while guard[0] <= x_max and guard[1] <= y_max and guard[0] >= 0 and guard[1] >= 0:
-    next_loc = None
-    match guard_moving:
-        case 90:
-            next_loc = (guard[0] - 1, guard[1])
-        case 180:
-            next_loc = (guard[0], guard[1] + 1)
-        case 270:
-            next_loc = (guard[0] + 1, guard[1])
-        case 0:
-            next_loc = (guard[0], guard[1] - 1)
-
-    # check if we are intersecting a previous path
-    if guard in positions and next_loc not in obstacles:
-        new_obstacles.add(next_loc)
-        print(next_loc)
-    
-    if next_loc in obstacles:
-        guard_moving += 90
-        if guard_moving == 360:
-            guard_moving = 0
-    else:
+def is_loop(obstacles):
+    # now move around the guard -- "play the game"
+    positions = set()
+    guard_moving = 90
+    guard = start
+    repeats = 0
+    while guard[0] <= x_max and guard[1] <= y_max and guard[0] >= 0 and guard[1] >= 0:
         positions.add(guard)
-        guard = next_loc
-
+        next_loc = None
+        match guard_moving:
+            case 90:
+                next_loc = (guard[0] - 1, guard[1])
+            case 180:
+                next_loc = (guard[0], guard[1] + 1)
+            case 270:
+                next_loc = (guard[0] + 1, guard[1])
+            case 0:
+                next_loc = (guard[0], guard[1] - 1)
         
+        if next_loc in obstacles:
+            guard_moving += 90
+            if guard_moving == 360:
+                guard_moving = 0
+        else:
+            guard = next_loc
 
-print('Part 2')
-print(len(new_obstacles))
+        if guard in positions:
+            repeats += 1
+            if repeats > 10000:
+                return True
+        else:
+            repeats = 0
+    return False
+
+loops = 0
+for x in range(len(puzzle_input)):
+    print(x, len(puzzle_input))
+    for y in range(len(puzzle_input[0])):
+        if (x, y) not in original_obstacles:
+            if is_loop([(x, y)] + original_obstacles):
+                loops += 1
+
+print(loops)
+
+
+
+
+
+
 
     
---
+++ 07/part1.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from pprint import pprint
+from time import sleep
+from typing import List
+
+puzzle_input = open('./07/input', encoding='utf-8').read().splitlines()
+
+
+def is_equation_true(result: int, numbers: List[int]) -> bool:
+    # Base case, if math exceeds expected result those operators can't be right
+    if numbers[0] > result:
+        return False
+    # additional base case, if length is 1, we can't do any more math, return if result is correct
+    elif len(numbers) == 1:
+        return numbers[0] == result
+    
+    # recursive case - addition
+    add_result = is_equation_true(result, [numbers[0] + numbers[1]] + numbers[2:])
+    mul_result = is_equation_true(result, [numbers[0] * numbers[1]] + numbers[2:])
+
+    return add_result or mul_result
+
+
+total = 0
+for line in puzzle_input:
+    # parse each line
+    result, numbers = line.split(': ')
+    result = int(result)
+    numbers = [int(x) for x in numbers.split(' ')]
+
+    # compute
+    if is_equation_true(result, numbers):
+        # print(result, numbers)
+        total += result
+
+print('Part 1')
+print(total)
